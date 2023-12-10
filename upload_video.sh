6729cc9f1fef047817a7ef43dbca7b5faac6a610
hibiki <hibiki123123@gmail.com>
2023-12-10 20:07:31 +0900

    リファクタリング5555

--- upload_video.sh
+++ upload_video.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-$cmd = 'python3 lib/youtube/youtube.py --file="save/movie/output_video.mp4" --title="Template Movie title" --description="Template description." --category="10" --privacyStatus="private"'
+$cmd='python3 lib/youtube/youtube.py --file="save/movie/output.mp4" --title="Template Movie title" --description="Template description." --category="10" --privacyStatus="private"'
 echo "-----------------------------------"
 echo $cmd
 echo "-----------------------------------"
